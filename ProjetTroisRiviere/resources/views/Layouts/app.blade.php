606e02c5e08919606afe981fd478533f56bc7ebd
Naijel2103 <loick.michaud2103@gmail.com>
2024-08-19 11:20:10 -0400

    layout css

--- ProjetTroisRiviere/resources/views/Layouts/app.blade.php
+++ ProjetTroisRiviere/resources/views/Layouts/app.blade.php
@@ -1,13 +1,33 @@
 <!DOCTYPE html>
 <html lang="fr-CA">
 <head>
+<meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <title> @yield('titre') </title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+        <script defer src="https://use.fontawesome.com/releases/v5.1.0/js/all.js" integrity="sha384-3LK/3kTpDE/Pkp8gTNp2gR/2gOiwQ6QaO7Td0zV76UFJVhqLl4Vl3KL1We6q6wR9" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" style="text/css" href="\css\GabaritCss\GabaritCss.css">
 <meta charset="UTF-8">
 </head>
 
 <body>
 <!-- Mettre la NavBar et toutes les entêtes du site ici -->
+<header>
+    
+      <nav class="main-nav">                
+        <a href="#Acceuil">Home</a>
 
+        <a href="#originals">Originals</a>
+        <a href="#">Recently Added</a>     
+      </nav>
+      <nav class="sub-nav">
+        <a href="#"><i class="fas fa-search sub-nav-logo"></i></a>
+        <a href="#"><i class="fas fa-bell sub-nav-logo"></i></a>
+   
+      </nav>      
+    </header>
 @yield('contenu')
 
 <!-- Mettre le footer -->
